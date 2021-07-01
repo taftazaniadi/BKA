f7baa23d853d4f0bd9bcae27ac62d92d4bad27c1
M Taftazani Adi <m.taftazani123@gmail.com>
2021-07-01 21:45:38 +0700

    Push to Repo BKA

---
+++ app/src/main/java/com/buka/amanah/home/MainActivity.java
@@ -0,0 +1,16 @@
+package com.buka.amanah.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.buka.amanah.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/buka/amanah/register/RegisterActivity.java
@@ -0,0 +1,53 @@
+package com.buka.amanah.register;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.buka.amanah.R;
+import com.buka.amanah.login.LoginActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        /* Toolbar */
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setElevation(0);
+
+        ActionBar actionbar = getSupportActionBar();
+//        actionbar.setIcon(R.drawable.ic_jitu);
+        actionbar.setTitle(Html.fromHtml("<b>PENDAFTAR</b>"));
+
+        // making notification bar transparent
+        changeStatusBarColor();
+    }
+
+    /**
+     * Making notification bar transparent
+     */
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    public void userLogin(View view) {
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+}
